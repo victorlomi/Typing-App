80898fa17940ac368f5f10a7c51b4ec160deaaeb
victorlomi <victorlominyo@gmail.com>
2020-12-17 08:22:48 +0300

    update user model by removing database related stuff and then update requirements.txt

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,16 @@
 from flask import Flask
 from config import Config
 
+from flask_login import LoginManager
+
+login = LoginManager()
+
+
 def create_app(config_class=Config):
     app = Flask(__name__)
     app.config.from_object(config_class)
+
+    login.init_app(app)
 
     from app.main import bp as main_bp
     app.register_blueprint(main_bp)
