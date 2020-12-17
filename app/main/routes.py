52678f214632a31f301ff59183e4647639cb2a17
ZephonMakale <mzephon@gmail.com>
2020-12-17 15:49:09 +0300

    modified backend.html, login.html and signup.html

--- app/main/routes.py
+++ app/main/routes.py
@@ -1,6 +1,12 @@
 from flask import render_template, redirect, url_for
+from flask_login import current_user
 from app.main import bp
 
 @bp.route('/')
 def index():
     return render_template('index.html')
+
+@bp.route('/backend')
+def backend():
+    return render_template('backend.html')
+
