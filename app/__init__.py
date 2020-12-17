796cbf4277be310f8ab1312ae36804479c5ae629
ZephonMakale <mzephon@gmail.com>
2020-12-17 15:46:10 +0300

    initialisation of the app

--- app/__init__.py
+++ app/__init__.py
@@ -4,11 +4,8 @@ from flask_bootstrap import Bootstrap
 
 from flask_login import LoginManager
 
-
 bootstrap = Bootstrap()
 login = LoginManager()
-
-
 
 def create_app(config_class=Config):
     app = Flask(__name__)
@@ -16,6 +13,7 @@ def create_app(config_class=Config):
 
     # Initializing Flask Extensions
     bootstrap.init_app(app)
+    login.init_app(app)
     
     from app.main import bp as main_bp
     app.register_blueprint(main_bp)
